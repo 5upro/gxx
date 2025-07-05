2d7736f5ea710186ed8af677cd15f1378cc05fd5
SUPRØ <00youareworngtho00@gmail.com>
2025-07-06 00:23:51 +0530

    fixed selfcmp() & tested DJB2

--- auxl.h
+++ auxl.h
@@ -6,9 +6,11 @@
 #define endl "\n"
 
 int selfcmp(const char *a, const char *b){
-	for(int i = 0; a[i] != '\0'; i++){
-        if(a[i] != b[i]) return 0;
-	}
+    int i=0;
+    while(a[i]!='\0'&& b[i]!='\0'){
+        if((a[i]!=b[i])) return 0;
+        i++;
+    }
     return 1;
 }
 
